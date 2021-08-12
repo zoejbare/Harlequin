5133fc694b2b9fa1e96aa040d543351f66beb166
Zoe Bare <zoe.j.bare@gmail.com>
2021-08-11 19:15:58 -0500

    Fixed a comment and the disassembly string for the STORE_MEMBER opcode

--- src/lib/runtime/op-impl/Store/StoreMember.cpp
+++ src/lib/runtime/op-impl/Store/StoreMember.cpp
@@ -30,11 +30,11 @@
 //
 // Set the value in a general-purpose register to an I/O register.
 //
-// 0x: STORE_MEMBER r#, r#, i#
+// 0x: STORE_MEMBER r#, r#, ##
 //
 //   r# [first]  = General-purpose register index that contains the object where the member will be stored
 //   r# [second] = General-purpose register index of the value to store on the object
-//   i#          = Immediate integer used as the direct index into the object's member table
+//   ##          = Immediate integer used as the direct index into the object's member table
 //
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -95,7 +95,7 @@ void OpCodeDisasm_StoreMember(XenonDisassemble& disasm)
 	const uint32_t memberIndex = XenonDecoder::LoadUint32(disasm.decoder);
 
 	char str[64];
-	snprintf(str, sizeof(str), "STORE_MEMBER r%" PRIu32 ", r%" PRIu32 ", i%" PRIu32, gpDstRegIndex, gpSrcRegIndex, memberIndex);
+	snprintf(str, sizeof(str), "STORE_MEMBER r%" PRIu32 ", r%" PRIu32 ", #%" PRIu32, gpDstRegIndex, gpSrcRegIndex, memberIndex);
 	disasm.onDisasmFn(disasm.pUserData, str, disasm.opcodeOffset);
 }
 
