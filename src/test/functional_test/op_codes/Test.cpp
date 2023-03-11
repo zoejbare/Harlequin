7246223645ac9d808fc6c5708b3594d3034dd375
Zoe Bare <zoe.j.bare@gmail.com>
2023-03-10 23:46:29 -0600

    Added TestOpCodes functional tests for the NOP and YIELD opcodes Consolidated the function serializer setup and tear down code for the TestOpCodes functional tests

--- src/test/functional_test/op_codes/Test.cpp
+++ src/test/functional_test/op_codes/Test.cpp
@@ -22,6 +22,13 @@
 
 //----------------------------------------------------------------------------------------------------------------------
 
+namespace Function
+{
+	constexpr const char* const main = "void main()";
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
 struct ExecStatus
 {
 	bool yield;
@@ -30,6 +37,41 @@ struct ExecStatus
 	bool exception;
 	bool abort;
 };
+
+//----------------------------------------------------------------------------------------------------------------------
+
+void SetupFunctionSerializer(HqSerializerHandle& output, const int endianness)
+{
+	// Create a serializer for the function.
+	HqSerializerHandle hFuncSerializer = HQ_SERIALIZER_HANDLE_NULL;
+	const int createFuncSerializerResult = HqSerializerCreate(&hFuncSerializer, HQ_SERIALIZER_MODE_WRITER);
+	ASSERT_EQ(createFuncSerializerResult, HQ_SUCCESS);
+
+	// Set the file serializer to the system's native endianness.
+	const int setFuncSerializerEndiannessResult = HqSerializerSetEndianness(hFuncSerializer, endianness);
+	ASSERT_EQ(setFuncSerializerEndiannessResult, HQ_SUCCESS);
+
+	output = hFuncSerializer;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+void FinalizeFunctionSerializer(
+	HqSerializerHandle hSerializer,
+	HqModuleWriterHandle hModuleWriter,
+	const char* const functionSignature)
+{
+	const void* const pFuncData = HqSerializerGetRawStreamPointer(hSerializer);
+	const size_t funcLength = HqSerializerGetStreamLength(hSerializer);
+
+	// Add the function to the module writer.
+	const int addFunctionResult = HqModuleWriterAddFunction(hModuleWriter, functionSignature, pFuncData, funcLength, 0, 0);
+	ASSERT_EQ(addFunctionResult, HQ_SUCCESS);
+
+	// Dispose of the function serializer.
+	const int disposeFuncSerializerResult = HqSerializerDispose(&hSerializer);
+	ASSERT_EQ(disposeFuncSerializerResult, HQ_SUCCESS);
+}
 
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -72,20 +114,65 @@ TEST(_HQ_TEST_NAME(TestOpCodes), Return)
 
 //----------------------------------------------------------------------------------------------------------------------
 
-TEST(_HQ_TEST_NAME(TestOpCodes), Abort)
+TEST(_HQ_TEST_NAME(TestOpCodes), Nop)
 {
-	static const char* const mainFuncSig = "void main()";
-
 	auto compilerCallback = [](HqModuleWriterHandle hModuleWriter, int endianness)
 	{
-		// Create a serializer for the function.
 		HqSerializerHandle hFuncSerializer = HQ_SERIALIZER_HANDLE_NULL;
-		const int createFuncSerializerResult = HqSerializerCreate(&hFuncSerializer, HQ_SERIALIZER_MODE_WRITER);
-		ASSERT_EQ(createFuncSerializerResult, HQ_SUCCESS);
 
-		// Set the file serializer to the system's native endianness.
-		const int setFuncSerializerEndiannessResult = HqSerializerSetEndianness(hFuncSerializer, endianness);
-		ASSERT_EQ(setFuncSerializerEndiannessResult, HQ_SUCCESS);
+		// Set the function serializer.
+		SetupFunctionSerializer(hFuncSerializer, endianness);
+
+		// Write the instruction that we're going to test.
+		const int writeNopInstrResult = HqBytecodeWriteNop(hFuncSerializer);
+		ASSERT_EQ(writeNopInstrResult, HQ_SUCCESS);
+
+		// Write a RETURN instruction just in case something goes wrong.
+		const int writeReturnInstrResult = HqBytecodeWriteReturn(hFuncSerializer);
+		ASSERT_EQ(writeReturnInstrResult, HQ_SUCCESS);
+
+		// Finalize the serializer and add it to the module.
+		FinalizeFunctionSerializer(hFuncSerializer, hModuleWriter, Function::main);
+	};
+
+	auto runtimeCallback = [](HqVmHandle hVm, HqExecutionHandle hExec)
+	{
+		(void) hVm;
+
+		// Run the execution context.
+		const int execRunResult = HqExecutionRun(hExec, HQ_RUN_FULL);
+		ASSERT_EQ(execRunResult, HQ_SUCCESS);
+
+		// Get the status of the execution context.
+		ExecStatus status;
+		GetExecutionStatus(status, hExec);
+		ASSERT_FALSE(status.yield);
+		ASSERT_FALSE(status.running);
+		ASSERT_TRUE(status.complete);
+		ASSERT_FALSE(status.exception);
+		ASSERT_FALSE(status.abort);
+	};
+
+	std::vector<uint8_t> bytecode;
+
+	// Construct the module bytecode for the test.
+	CompileBytecode(bytecode, compilerCallback);
+	ASSERT_GT(bytecode.size(), 0u);
+
+	// Run the module bytecode.
+	ProcessBytecode("TestOpCodes", Function::main, runtimeCallback, bytecode);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+TEST(_HQ_TEST_NAME(TestOpCodes), Abort)
+{
+	auto compilerCallback = [](HqModuleWriterHandle hModuleWriter, int endianness)
+	{
+		HqSerializerHandle hFuncSerializer = HQ_SERIALIZER_HANDLE_NULL;
+
+		// Set the function serializer.
+		SetupFunctionSerializer(hFuncSerializer, endianness);
 
 		// Write the instruction that we're going to test.
 		const int writeAbortInstrResult = HqBytecodeWriteAbort(hFuncSerializer);
@@ -95,16 +182,8 @@ TEST(_HQ_TEST_NAME(TestOpCodes), Abort)
 		const int writeReturnInstrResult = HqBytecodeWriteReturn(hFuncSerializer);
 		ASSERT_EQ(writeReturnInstrResult, HQ_SUCCESS);
 
-		const void* const pFuncData = HqSerializerGetRawStreamPointer(hFuncSerializer);
-		const size_t funcLength = HqSerializerGetStreamLength(hFuncSerializer);
-
-		// Add the function to the module writer.
-		const int addFunctionResult = HqModuleWriterAddFunction(hModuleWriter, mainFuncSig, pFuncData, funcLength, 0, 0);
-		ASSERT_EQ(addFunctionResult, HQ_SUCCESS);
-
-		// Dispose of the function serializer.
-		const int disposeFuncSerializerResult = HqSerializerDispose(&hFuncSerializer);
-		ASSERT_EQ(disposeFuncSerializerResult, HQ_SUCCESS);
+		// Finalize the serializer and add it to the module.
+		FinalizeFunctionSerializer(hFuncSerializer, hModuleWriter, Function::main);
 	};
 
 	auto runtimeCallback = [](HqVmHandle hVm, HqExecutionHandle hExec)
@@ -132,7 +211,58 @@ TEST(_HQ_TEST_NAME(TestOpCodes), Abort)
 	ASSERT_GT(bytecode.size(), 0u);
 
 	// Run the module bytecode.
-	ProcessBytecode("TestOpCodes", mainFuncSig, runtimeCallback, bytecode);
+	ProcessBytecode("TestOpCodes", Function::main, runtimeCallback, bytecode);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+TEST(_HQ_TEST_NAME(TestOpCodes), Yield)
+{
+	auto compilerCallback = [](HqModuleWriterHandle hModuleWriter, int endianness)
+	{
+		HqSerializerHandle hFuncSerializer = HQ_SERIALIZER_HANDLE_NULL;
+
+		// Set the function serializer.
+		SetupFunctionSerializer(hFuncSerializer, endianness);
+
+		// Write the instruction that we're going to test.
+		const int writeYieldInstrResult = HqBytecodeWriteYield(hFuncSerializer);
+		ASSERT_EQ(writeYieldInstrResult, HQ_SUCCESS);
+
+		// Write a RETURN instruction just in case something goes wrong.
+		const int writeReturnInstrResult = HqBytecodeWriteReturn(hFuncSerializer);
+		ASSERT_EQ(writeReturnInstrResult, HQ_SUCCESS);
+
+		// Finalize the serializer and add it to the module.
+		FinalizeFunctionSerializer(hFuncSerializer, hModuleWriter, Function::main);
+	};
+
+	auto runtimeCallback = [](HqVmHandle hVm, HqExecutionHandle hExec)
+	{
+		(void) hVm;
+
+		// Run the execution context.
+		const int execRunResult = HqExecutionRun(hExec, HQ_RUN_FULL);
+		ASSERT_EQ(execRunResult, HQ_SUCCESS);
+
+		// Get the status of the execution context.
+		ExecStatus status;
+		GetExecutionStatus(status, hExec);
+		ASSERT_TRUE(status.yield);
+		ASSERT_TRUE(status.running);
+		ASSERT_FALSE(status.complete);
+		ASSERT_FALSE(status.exception);
+		ASSERT_FALSE(status.abort);
+	};
+
+	std::vector<uint8_t> bytecode;
+
+	// Construct the module bytecode for the test.
+	CompileBytecode(bytecode, compilerCallback);
+	ASSERT_GT(bytecode.size(), 0u);
+
+	// Run the module bytecode.
+	ProcessBytecode("TestOpCodes", Function::main, runtimeCallback, bytecode);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
