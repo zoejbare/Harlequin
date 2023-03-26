a5d23d5bd0214dd9cbf8ab795fc7f4ef1e2d3cc4
Zoe Bare <zoe.j.bare@gmail.com>
2023-03-26 10:27:27 -0500

    Improve the test name macro with better granularity in the names

--- src/test/common/Util.h
+++ src/test/common/Util.h
@@ -25,23 +25,71 @@
 //----------------------------------------------------------------------------------------------------------------------
 
 #if defined(_HQ_CONFIG_DEBUG)
-	#define __HQ_TEST_NAME_PREFIX Dbg_
+	#define __HQ_TEST_NAME_CONFIG _dbg
 
 #elif defined(_HQ_CONFIG_FASTDEBUG)
-	#define __HQ_TEST_NAME_PREFIX FDbg_
+	#define __HQ_TEST_NAME_CONFIG _fdbg
 
 #elif defined(_HQ_CONFIG_RELEASE)
-	#define __HQ_TEST_NAME_PREFIX Rel_
+	#define __HQ_TEST_NAME_CONFIG _rel
 
 #else
 	#error Unknown build configuration
 
 #endif
 
-#define __HQ_NAME_CONCAT_EVAL2(prefix, name) prefix ## name
-#define __HQ_NAME_CONCAT_EVAL1(prefix, name) __HQ_NAME_CONCAT_EVAL2(prefix, name)
+//----------------------------------------------------------------------------------------------------------------------
 
-#define _HQ_TEST_NAME(test_name) __HQ_NAME_CONCAT_EVAL1(__HQ_TEST_NAME_PREFIX, test_name)
+#if defined(HQ_CPU_TYPE_X86)
+	#if defined(HQ_CPU_WORD_64_BIT)
+		#define __HQ_TEST_NAME_ARCH _x64
+	#elif defined(HQ_CPU_WORD_32_BIT)
+		#define __HQ_TEST_NAME_ARCH _x86
+	#endif
+#elif defined(HQ_CPU_TYPE_PPC)
+	#if defined(HQ_CPU_WORD_64_BIT)
+		#define __HQ_TEST_NAME_ARCH _ppc64
+	#elif defined(HQ_CPU_WORD_32_BIT)
+		#define __HQ_TEST_NAME_ARCH _ppc
+	#endif
+#elif defined(HQ_CPU_TYPE_ARM)
+	#if defined(HQ_CPU_WORD_64_BIT)
+		#define __HQ_TEST_NAME_ARCH _arm64
+	#elif defined(HQ_CPU_WORD_32_BIT)
+		#define __HQ_TEST_NAME_ARCH _arm
+	#endif
+
+#endif
+
+//----------------------------------------------------------------------------------------------------------------------
+
+#if defined(__clang__)
+	#define __HQ_TEST_NAME_COMPILER _clang
+
+#elif defined(__GNUC__)
+	#define __HQ_TEST_NAME_COMPILER _gcc
+
+#elif defined(__CC_ARM)
+	#define __HQ_TEST_NAME_COMPILER _armcc
+
+#elif defined(_MSC_VER)
+	#define __HQ_TEST_NAME_COMPILER _msvc
+
+#elif defined(__INTEL_COMPILER)
+	#define __HQ_TEST_NAME_COMPILER _icc
+
+#else
+	#error Unknown compiler
+
+#endif
+
+//----------------------------------------------------------------------------------------------------------------------
+
+#define __HQ_NAME_CONCAT_EVAL2(name, config, arch, compiler) name ## config ## arch ## compiler
+#define __HQ_NAME_CONCAT_EVAL1(name, config, arch, compiler) __HQ_NAME_CONCAT_EVAL2(name, config, arch, compiler)
+
+#define _HQ_TEST_NAME(test_name) \
+	__HQ_NAME_CONCAT_EVAL1(test_name, __HQ_TEST_NAME_CONFIG, __HQ_TEST_NAME_ARCH, __HQ_TEST_NAME_COMPILER)
 
 //----------------------------------------------------------------------------------------------------------------------
 
